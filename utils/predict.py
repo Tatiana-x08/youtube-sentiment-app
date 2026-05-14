1cefe8bc39d4d0953ad0d2928d30649cabd1009f
Nour <nhajbi11@gmail.com>
2026-05-14 20:09:16 +0200

    predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -8,27 +8,31 @@ def predecir_sentimiento(texto):
     texto_lower = texto.lower()
 
     positivas = [
-        "love", "like", "amazing", "good", "great", "excellent",
-        "me encanta", "me gusta", "bueno", "genial", "excelente"
+        "love", "amazing", "great", "excellent", "best",
+        "me encanta", "genial", "excelente"
     ]
 
     negativas = [
-        "hate", "bad", "terrible", "horrible", "awful",
-        "odio", "mierda", "malo", "fatal", "asco"
+        "hate", "terrible", "worst", "horrible", "bad",
+        "mierda", "horrible", "malo"
     ]
 
+    # reglas rápidas
     if any(p in texto_lower for p in positivas):
         return "😊 Positivo", 0.95
 
     if any(n in texto_lower for n in negativas):
         return "😡 Negativo", 0.95
 
+    # KNN real
     tokens = texto_lower.split()
+
     vector = modelo.infer_vector(tokens)
 
     pred = knn.predict([vector])[0]
 
-    if pred == 0:
+    # CLASES INVERTIDAS
+    if pred == 1:
         resultado = "😊 Positivo"
     else:
         resultado = "😡 Negativo"
